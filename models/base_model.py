c6fa9aa35417665866fcf14fba1109609ba16696
Oliyad Kebede <81267448+OliyadKebede@users.noreply.github.com>
2022-08-07 16:32:58 -0700

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,83 +1,67 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""Base Model Module
-This module is in charge of establishing a reference
-Base Model for the rest of the classes of the
-HBNB project (Airbnb Clone), from which it will be possible
-to extract information such as: A unique universal identifier,
-the date and time in which a class was created and updated,
-a standard format to print the class content, a way to save
-the data created from the instances and finally the representation
-of all the keys and values of an instance.
+"""
+base class file
 """
 
+import uuid
 from datetime import datetime
 import models
-import uuid
+from json import JSONEncoder
 
 
 class BaseModel:
-    """Base Model Class
-    This is the Base Model that take care of the
-    initialization, serialization and deserialization
-    of the future instances.
-    Attributes:
-        id (str): It's an UUID for when an instance is created.
-        created_at (datetime): The current date and time that
-            an instance is created.
-        updated_at (datetime): The current date and time that
-            an instance is created and it will be updated every
-            time that the object changes.
+    """
+    =========
+    BaseModel
+    =========
     """
 
     def __init__(self, *args, **kwargs):
-        """Base Model __init__ Method
-        Here, the default values of a Base Model
-        instance are initialized.
-        """
+        """initialize the instance of the class"""
         if kwargs:
-            for arg, val in kwargs.items():
-                if arg in ('created_at', 'updated_at'):
-                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "__class__":
+                    continue
 
-                if arg != '__class__':
-                    setattr(self, arg, val)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """Representation of the class for the user
-        Example:
-            $ bm = BaseModel()
-            $ print(bm)
-            This method prints the content of the Base Model
-            class with this format
-            $ [<class name>] (<self.id>) <self.__dict__>
-        """
-        return '[{0}] ({1}) {2}'.format(
-                self.__class__.__name__, self.id, self.__dict__
-            )
-
     def save(self):
-        """Updates a Base Model instance
-        Updates the public instance attribute `updated_at`
-        with the current datetime and dumps the class data
-        into a file
-        """
+        """save new informations to the class object"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Converts the information of the class to human-readable format
-        Returns a new dictionary containing all keys/values
-        of __dict__ of the instance.
-        """
-        class_info = self.__dict__.copy()
-        class_info['__class__'] = self.__class__.__name__
-        class_info['created_at'] = self.created_at.isoformat()
-        class_info['updated_at'] = self.updated_at.isoformat()
+        """return dictionary representaton of the instance"""
+        dict_repr = {}
+        for key, value in self.__dict__.items():
+            dict_repr[key] = value
+            if isinstance(value, datetime):
+                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dict_repr["__class__"] = type(self).__name__
+        return dict_repr
 
-        return class_info
+    def __str__(self):
+        """return the string formated message when instance is called"""
+        clName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
+
+
+class BaseModelEncoder(JSONEncoder):
+    """JSON Encoder for BaseModel
+    """
+
+    def default(self, o):
+        """ default"""
+        if isinstance(o, BaseModel):
+            return o.to_dict()
+        return super().default(o)
